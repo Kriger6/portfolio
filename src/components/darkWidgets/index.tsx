fe67badd9374af09187fcc7d85d68665d400f512
Kriger6 <sasa.palinkas6@gmail.com>
2023-07-20 21:51:52 +0200

    grid keys fixed

--- src/components/darkWidgets/index.tsx
+++ src/components/darkWidgets/index.tsx
@@ -1,7 +1,7 @@
-
+import {v4 as uuidv4} from 'uuid'
 const DarkWidgets = () => {
 
-    let grid = Array(100).fill(<div style={{ width: "50px", height: "50px", border: "1px solid #292929", backgroundColor: "#171717"}}></div>)
+    let grid = Array(100).fill(undefined).map(() => <div key={uuidv4()} style={{ width: "50px", height: "50px", border: "1px solid #292929", backgroundColor: "#171717" }}></div>)
 
     return (
         <div style={{ display: "flex", width: "500px", height: "500px", flexWrap: "wrap" }}>
