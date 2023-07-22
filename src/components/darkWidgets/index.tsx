4224d4d6b936a842f21baa417629e33cda844297
Kriger6 <sasa.palinkas6@gmail.com>
2023-07-22 20:40:23 +0200

    navbar z-index fixed

--- src/components/darkWidgets/index.tsx
+++ src/components/darkWidgets/index.tsx
@@ -1,11 +1,15 @@
 import {v4 as uuidv4} from 'uuid'
+import './index.css'
 const DarkWidgets = () => {
 
     let grid = Array(100).fill(undefined).map(() => <div key={uuidv4()} style={{ width: "50px", height: "50px", border: "1px solid #292929", backgroundColor: "#171717" }}></div>)
 
     return (
-        <div style={{ display: "flex", width: "500px", height: "500px", flexWrap: "wrap" }}>
-            {grid}            
+        <div className='widget-container' style={{ display: "flex", width: "500px", height: "500px", flexWrap: "wrap" }}>
+            {grid}
+            {/* <div>
+
+            </div>             */}
         </div>
     )
 }
