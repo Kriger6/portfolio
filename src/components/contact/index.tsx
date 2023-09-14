5b0ac78e72c26ed8864c365010800a5a5f72fff3
Kriger6 <sasa.palinkas6@gmail.com>
2023-09-14 20:49:07 +0200

    more UI done

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -16,21 +16,25 @@ const Contact = ({sectionRef}: any) => {
                     <form name="contact" id="contact-form">
                         <div>
                             <label className="contact-label" htmlFor="contact-form">What's your name?
-                                <input placeholder="Saša Palinkaš" type="text" name="name" id="" />
+                                <input required placeholder="Saša Palinkaš" type="text" name="name" id="" />
                             </label>
                         </div>
                         <div>
                             <label className="contact-label" htmlFor="contact-form">Where can i reach you?
-                                <input placeholder="sasa.palinkas6@gmail.com" type="email" name="email" id="" />
+                                <input required placeholder="sasa.palinkas6@gmail.com" type="email" name="email" id="" />
                             </label>
                         </div>
                         <div>
                             <label className="contact-label" htmlFor="contact-form">What's your message?
-                                <textarea placeholder="Hi Saša! Lets work!" name="message"></textarea>
+                                <textarea required placeholder="Hi Saša! Lets work!" name="message"></textarea>
                             </label>
                         </div>
                         <div>
-                            <button className="submit-button" type="submit">Send it</button>
+                            <button className="submit-button" type="submit">Send it
+                                <svg className="w-6 h-6 text-gray-800 dark:text-white" style={{marginLeft: '15px'}} width={'25px'} height={'25px'} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+                                    <path stroke="#b5b5b5" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.5" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
+                                </svg>
+                            </button>
                         </div>
                     </form>
                 </div>
